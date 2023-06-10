a64119b0176282da1164e836f4abb0fe40623735
JT <60622543+HarryFishnuts@users.noreply.github.com>
2023-06-10 18:43:25 -0400

    Working on math module

---
+++ ct_math.h
@@ -0,0 +1,67 @@
+//////////////////////////////////////////////////////////////////////////////
+///	
+/// 							<ct_math.h>
+///								Bailey JT Brown
+///								2023
+/// 
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef _CT_MATH_INCLUDE_
+#define _CT_MATH_INCLUDE_
+
+#include "ct_base.h"
+
+//////////////////////////////////////////////////////////////////////////////
+///
+///								2D VECTOR OPERATIONS
+/// 
+//////////////////////////////////////////////////////////////////////////////
+
+#define CT_RADIANS_TO_DEGREES	57.2958f
+#define CT_DEGREES_TO_RADIANS	0.01745f
+
+typedef struct CTVect {
+	FLOAT x, y;
+} CTVect, PCTVect;
+
+CTCALL	CTVect	CTMakeVect(FLOAT x, FLOAT y);
+CTCALL	CTVect	CTVectAdd(CTVect v1, CTVect v2);
+CTCALL	CTVect	CTVectSubtract(CTVect v1, CTVect v2);
+CTCALL	CTVect	CTVectMultiply(CTVect vect, FLOAT scalar);
+CTCALL	FLOAT	CTVectDot(CTVect v1, CTVect v2);
+CTCALL	CTVect	CTVectNormalize(CTVect vect);
+CTCALL	FLOAT	CTVectMagnitude(CTVect vect);
+CTCALL	FLOAT	CTVectMagnitudeSqr(CTVect vect);
+CTCALL	FLOAT	CTVectMagnitudeFast(CTVect vect);
+CTCALL	void	CTVectComponents(CTVect vect, PFLOAT x, PFLOAT y);
+CTCALL	FLOAT	CTVectAngleDegrees(CTVect vect);
+
+//////////////////////////////////////////////////////////////////////////////
+///
+///							3x3 AFFINE TRANSFORM MATRIX
+/// 
+//////////////////////////////////////////////////////////////////////////////
+
+typedef struct CTMatrix {
+	FLOAT vals[3][3];
+} CTMatrix, *PCTMatrix;
+
+CTCALL	CTMatrix	CTMatrixIdentity();
+CTCALL	CTMatrix	CTMatrixMultiply(CTMatrix m1, CTMatrix m2);
+CTCALL	CTMatrix	CTMatrixScale(CTMatrix mat, CTVect scl);
+CTCALL	CTMatrix	CTMatrixTranslate(CTMatrix mat, CTVect trl);
+CTCALL	CTMatrix	CTMatrixRotate(CTMatrix mat, FLOAT rotation);
+CTCALL	CTVect		CTMatrixApply(CTMatrix mat, CTVect vect);
+
+//////////////////////////////////////////////////////////////////////////////
+///
+///								RANDOM
+/// 
+//////////////////////////////////////////////////////////////////////////////
+
+CTCALL	UINT32		CTRandomSeed(void);
+CTCALL	UINT32		CTRandomInt(UINT32 seed);
+CTCALL	FLOAT		CTRandomFloat(FLOAT seed);
+CTCALL	BOOL		CTRandomChance(UINT32 seed, UINT32 num, UINT32 denom);
+
+#endif
---
+++ ct_math_vector.c
@@ -0,0 +1,88 @@
+//////////////////////////////////////////////////////////////////////////////
+///	
+/// 							<ct_dllmain.c>
+///								Bailey JT Brown
+///								2023
+/// 
+//////////////////////////////////////////////////////////////////////////////
+
+#include "ct_math.h"
+
+#include <math.h>
+
+CTCALL	CTVect	CTMakeVect(FLOAT x, FLOAT y) {
+	CTVect rv = {
+		.x = x,
+		.y = y
+	};
+	return rv;
+}
+
+CTCALL	CTVect	CTVectAdd(CTVect v1, CTVect v2) {
+	CTVect rv = {
+		.x = v1.x + v2.x,
+		.y = v2.y + v2.y
+	};
+	return rv;
+}
+
+CTCALL	CTVect	CTVectSubtract(CTVect v1, CTVect v2) {
+	CTVect rv = {
+		.x = v1.x - v2.x,
+		.y = v2.y - v2.y
+	};
+	return rv;
+}
+
+CTCALL	CTVect	CTVectMultiply(CTVect vect, FLOAT scalar) {
+	CTVect rv = {
+		.x = vect.x * scalar,
+		.y = vect.y * scalar
+	};
+	return rv;
+}
+
+CTCALL	FLOAT	CTVectDot(CTVect v1, CTVect v2) {
+	return (v1.x * v2.x + v1.y * v2.y);
+}
+
+CTCALL	CTVect	CTVectNormalize(CTVect vect) {
+
+}
+
+CTCALL	FLOAT	CTVectMagnitude(CTVect vect) {
+	return sqrtf(vect.x * vect.x + vect.y * vect.y);
+}
+
+CTCALL	FLOAT	CTVectMagnitudeSqr(CTVect vect) {
+	return vect.x * vect.x + vect.y * vect.y;
+}
+
+CTCALL	FLOAT	CTVectMagnitudeFast(CTVect vect) {
+
+	// refer to <https://youtu.be/NWBEA2ECX-A> for details
+
+	FLOAT min, max;
+
+	if (vect.x > vect.y) {
+		max = vect.x;
+		min = vect.y;
+	}
+	else {
+		max = vect.y;
+		min = vect.x;
+	}
+
+	return max * 0.96f + min * 0.40f;
+}
+
+CTCALL	void	CTVectComponents(CTVect vect, PFLOAT x, PFLOAT y) {
+	if (x != NULL) 
+		*x = vect.x;
+	if (y != NULL)
+		*y = vect.y;
+}
+
+CTCALL	FLOAT	CTVectAngleDegrees(CTVect vect) {
+	return atan2f(vect.y, vect.x) * CT_RADIANS_TO_DEGREES;
+}
